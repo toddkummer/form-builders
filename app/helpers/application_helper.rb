4ff1a46ddd266d18669de3fc755106772b748462
Todd Kummer <todd@rockridgesolutions.com>
2026-01-22 12:37:30 -0800

    Skeleton method in ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  # build a helper to generate the date picker input field
+  #
+  # <div data-controller="datepicker">
+  #   <input data-datepicker-target="input" type="text" name="foobar" value="2022-03-23"/>
+  #   <span data-datepicker-target="toggle">calendar</span>
+  # </div>
+  def date_picker(form, method, options = {})
+  end
 end
