0f2263d440a1672bad082fc529b364f42a3c8010
Todd Kummer <todd@rockridgesolutions.com>
2026-01-22 13:14:34 -0800

    Add Font Awesome

--- app/components/layout.rb
+++ app/components/layout.rb
@@ -30,6 +30,11 @@ module Components
 
           link rel: "stylesheet",
                href: "https://unpkg.com/stimulus-datepicker@1.0.5/css/datepicker.css"
+          link rel: "stylesheet",
+               href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css",
+               integrity: "sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==",
+               crossorigin: "anonymous",
+               referrerpolicy: "no-referrer"
           stylesheet_link_tag :app, "data-turbo-track": "reload"
         end
 
