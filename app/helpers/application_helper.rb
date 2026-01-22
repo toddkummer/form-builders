a1331ba5bec7463e1a9bbf75a87c3831e1d12300
Todd Kummer <todd@rockridgesolutions.com>
2026-01-22 12:44:59 -0800

    Skeleton date_field override

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  # override the date_field helper to generate the html for the date picker
+  #
+  # <div data-controller="datepicker">
+  #   <input data-datepicker-target="input" type="text" name="foobar" value="2022-03-23"/>
+  #   <i class="fa-regular fa-calendar" data-datepicker-target="toggle"></i>
+  # </div>
+  def date_field(object_name, method, options = {})
+  end
 end
